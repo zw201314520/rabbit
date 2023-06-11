8cc3877c766abeba1438504a8c9d3823f85875c6
zhouwen <1066737270@qq.com>
2023-06-11 16:53:56 +0800

    Layout静态结构搭建

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,15 +1,12 @@
-<template>
-  <span>我是首页</span>
-  <!-- 二级路由出口 -->
-  <RouterView></RouterView>
-</template>
-
-<script>
-export default {
-
-}
+<script setup>
+import LayoutNav from './components/LayoutNav.vue'
+import LayoutHeader from './components/LayoutHeader.vue'
+import LayoutFooter from './components/LayoutFooter.vue'
 </script>
 
-<style>
-
-</style>
+<template>
+  <LayoutNav />
+  <LayoutHeader />
+  <RouterView />
+  <LayoutFooter />
+</template>
