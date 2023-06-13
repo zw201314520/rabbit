54acb53ceee92cc89eac45492a610f5348a3e0c7
zhouwen <1066737270@qq.com>
2023-06-13 15:06:54 +0800

    home-面板组建的封装

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -3,7 +3,9 @@ import HomeCategory from './components/HomeCategory.vue'
 import HomeBanner from './components/HomeBanner.vue'
 import HomeNew from './components/HomeNew.vue'
 import HomeHot from './components/HomeHot.vue'
-import homeProduct from './components/HomeProduct.vue'
+import HomeProduct from './components/HomeProduct.vue'
+import HomePanel from './components/HomePanel.vue'
+
 </script>
 
 <template>
@@ -13,5 +15,17 @@ import homeProduct from './components/HomeProduct.vue'
   </div>
   <HomeNew />
   <HomeHot />
-  <homeProduct />
+  <HomeProduct />
+  //测试面板组件
+  <HomePanel title="新鲜好物" sub-title="新鲜好物 好多商品">
+    <div>
+      新鲜好物的插槽内容
+    </div>
+  </HomePanel>
+
+  <HomePanel title="人气推荐" sub-title="人气推荐 好多商品">
+    <div>
+      人气推荐的插槽内容
+    </div>
+  </HomePanel>
 </template>
